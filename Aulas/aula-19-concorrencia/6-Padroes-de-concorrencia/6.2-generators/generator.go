b690ba15df128486195effd6bbca5c3c2a1de13d
Wiles <gianhabeck@gmail.com>
2022-09-09 17:14:23 -0300

    Novas aulas executadas

--- Aulas/aula-19-concorrencia/6-Padroes-de-concorrencia/6.2-generators/generator.go
+++ Aulas/aula-19-concorrencia/6-Padroes-de-concorrencia/6.2-generators/generator.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package main
 
 import (
@@ -25,32 +24,4 @@ func escrever(texto string) <-chan string {
 	}()
 
 	return canal
-=======
-package main
-
-import (
-	"fmt"
-	"time"
-)
-
-func main() {
-	canal := escrever("Olá Mundo!")
-
-	for i := 0; i < 10; i++ {
-		fmt.Println(<- canal)
-	}
-}
-
-func escrever(texto string) <-chan string {
-	canal := make(chan string)
-
-	go func() {
-		for {
-			canal <- fmt.Sprintf("Valor recebido %s", texto)
-			time.Sleep(time.Millisecond * 500)
-		}
-	}()
-
-	return canal
->>>>>>> 9a41db2f2f686b367023230ea880f1a970ac856c
 }
